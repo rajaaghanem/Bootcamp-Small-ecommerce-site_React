98097ebf1cac6e816baa298084877c1403550e67
rajaaghanem <rajaaghanem@gmail.com>
2021-12-23 21:45:36 +0200

    data

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Homepage.css"
+import homeStore from "../../stores/homeStore";
+import kidsStore from "../../stores/kidsStore";
+import menStore from "../../stores/menStore";
+import womenStore from "../../stores/womenStore";
+import "./Homepage.css";
 
-const Homepage=()=>{
-    return(
-        <div className="homepage-container">
-            
-        </div>
-    );
+class Homepage extends React.Component {
+  state = { homeData: [], womenData: [], menData: [], kidsData: [] };
+
+  componentDidMount = () => {
+    this.setState({
+      homeData: homeStore,
+      womenData: womenStore,
+      menData: menStore,
+      kidsData: kidsStore,
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return <div></div>;
+  }
 }
+
 export default Homepage;
