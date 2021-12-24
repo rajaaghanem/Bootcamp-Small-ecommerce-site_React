56c4233320506a5f0b0e3868e7ac757aeab54ec8
rajaaghanem <rajaaghanem@gmail.com>
2021-12-24 21:02:10 +0200

    add cart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
 import { Link } from "react-router-dom";
 
-
-const Header=()=>{
-    return(
-        <div className="header-container">
-            <Link className="link" to="/">Homepage</Link>
-            <Link className="link" to="/products">Products</Link>
+const Header = () => {
+  return (
+    <div className="header-container">
+      <Link className="link homepage-link" to="/">
+        HomePage
+      </Link>
+      <Link className="link" to="/products">
+        <div className="cart-container">
+          <p>0</p>
+          <i class="fas fa-shopping-cart"></i>
         </div>
-    );
-}
+      </Link>
+    </div>
+  );
+};
 export default Header;
